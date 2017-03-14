24244063957efc812d22fd93040397269af1728d
py-am-i <parkeryoung228@gmail.com>
2017-03-13 21:21:24 -0700

    Fixed import

--- server_plugins/Quit.py
+++ server_plugins/Quit.py
@@ -1,4 +1,4 @@
-from server import LoopController
+from common import LoopControl
 
 
 class Plugin(object):
@@ -9,19 +9,16 @@ class Plugin(object):
     def run(self, server, data):
         print('Detaching from client...')
 
-        lc = LoopController()
+        lc = LoopControl()
 
         try:
             server.connection_mgr.close()
         except (BrokenPipeError, OSError) as err_msg:
             server.connection_mgr.remove_connection(server.connection_mgr.current_connection)
             server.connection_mgr.current_connection = None
-
-            lc.should_break = True
-            return lc
+            return lc.should_break()
 
         server.connection_mgr.remove_connection(server.connection_mgr.current_connection)
         server.connection_mgr.current_connection = None
 
-        lc.should_break = True
-        return lc
+        return lc.should_break()
