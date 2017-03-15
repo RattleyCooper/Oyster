cdcb051bd29e41cac6b6fbfcef2a6d56c3853cf6
py-am-i <parkeryoung228@gmail.com>
2017-03-14 19:46:45 -0700

    Changed `-a` flag in `shell_plugins/Client.py` `Plugin` to `-b` for batch.  More in notes
    Refactored code so that the main `run` method isn’t as cluttered.

--- shell_plugins/Client.py
+++ shell_plugins/Client.py
@@ -15,49 +15,68 @@ class Plugin(object):
             print('Help doc coming soon.')
             return
 
-        # Send the command given after the -a flag to all connected clients.
-        if args[0] == '-a':
-            # Send command to all clients
-            args = shlex.split(data, posix=False)
+        if args[0] == '-b':
             try:
-                command = ' '.join(args[1:])
-                response = server.connection_mgr.send_commands(command)
+                return Plugin.dispatch_batch_command(server, ' '.join(args[1:]))
             except IndexError:
-                print('< You must include a command to send to the clients. >')
+                print('< You must include a command to send. >')
                 return
-            print(response, end='')
-            return response
 
         if args[0] == '-d':
-            """
-            Copy the client-specific files into the "dist" folder.
-            """
-            oyster_fp = __file__.replace('shell_plugins/', '').replace(__file__.split('/')[-1], '')
-            client_plugins_filepath = oyster_fp + 'client_plugins/'
-
-            client_plugin_filenames = [n for n in os.listdir(client_plugins_filepath)]
-            hidden_files = [n for n in os.listdir(client_plugins_filepath) if n[0] == '.']
-            client_plugin_filenames = [n for n in client_plugin_filenames if n not in hidden_files]
-
-            print('\r< Creating client distribution package... >')
-            try:
-                os.mkdir(oyster_fp + 'dist')
-                os.mkdir(oyster_fp + 'dist/client_plugins')
-            except FileExistsError:
-                pass
-
-            for filename in client_plugin_filenames:
-                destination = oyster_fp + 'dist/client_plugins/' + filename
-                origin = client_plugins_filepath + filename
-                copy(origin, destination)
-
-            copy(oyster_fp + '__init__.py', oyster_fp + 'dist/__init__.py')
-            copy(oyster_fp + 'common.py', oyster_fp + 'dist/common.py')
-            copy(oyster_fp + 'client.py', oyster_fp + 'dist/client.py')
-            copy(oyster_fp + 'update.py', oyster_fp + 'dist/update.py')
-
-            print('\r< Finished... >')
+            Plugin.handle_dist_flag()
             return
+
+    @staticmethod
+    def dispatch_batch_command(server, command):
+        """
+        Send the command given after the -a flag to all connected clients.
+
+        :param server:
+        :param command:
+        :return:
+        """
+
+        if not command:
+            print('< You must include a command. >')
+            return ''
+        response = server.connection_mgr.send_commands(command)
+        print(response, end='')
+        return response
+
+    @staticmethod
+    def handle_dist_flag():
+        """
+        Copy the client-specific files into the "dist" folder.
+
+        :return:
+        """
+
+        oyster_fp = __file__.replace('shell_plugins/', '').replace(__file__.split('/')[-1], '')
+        client_plugins_filepath = oyster_fp + 'client_plugins/'
+
+        client_plugin_filenames = [n for n in os.listdir(client_plugins_filepath)]
+        hidden_files = [n for n in os.listdir(client_plugins_filepath) if n[0] == '.']
+        client_plugin_filenames = [n for n in client_plugin_filenames if n not in hidden_files]
+
+        print('\r< Creating client distribution package... >')
+        try:
+            os.mkdir(oyster_fp + 'dist')
+            os.mkdir(oyster_fp + 'dist/client_plugins')
+        except FileExistsError:
+            pass
+
+        for filename in client_plugin_filenames:
+            destination = oyster_fp + 'dist/client_plugins/' + filename
+            origin = client_plugins_filepath + filename
+            copy(origin, destination)
+
+        copy(oyster_fp + '__init__.py', oyster_fp + 'dist/__init__.py')
+        copy(oyster_fp + 'common.py', oyster_fp + 'dist/common.py')
+        copy(oyster_fp + 'client.py', oyster_fp + 'dist/client.py')
+        copy(oyster_fp + 'update.py', oyster_fp + 'dist/update.py')
+
+        print('\r< Finished... >')
+        return
 
 
 if __name__ == '__main__':
