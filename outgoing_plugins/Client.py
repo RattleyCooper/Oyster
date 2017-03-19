113651ef9694af50a1cd6dc1b346594848dd4a72
py-am-i <parkeryoung228@gmail.com>
2017-03-18 20:34:07 -0700

    Added comments to `outgoing_plugins.Client`

--- outgoing_plugins/Client.py
+++ outgoing_plugins/Client.py
@@ -14,16 +14,21 @@ class Plugin(object):
             pass
 
         if args[0] == '-i':
+            # Tell the client we want an interactive python shell.
             server.connection_mgr.send_command('client -i', get_cwd=False)
             while True:
+                # Ask for a command to send to the client.
                 command = safe_input('>>> ')
+                # Handle the exit() command if needed.
                 if command == 'exit()':
                     server.connection_mgr.send_command(command, get_cwd=False)
                     break
+                # Send the command to the client and print the response..
                 response = server.connection_mgr.send_command(command, get_cwd=False)
                 if response.strip():
                     print(response.strip())
             return
 
+        # Send the command to the client if it doesn't need to be intercepted!
         print(server.connection_mgr.send_command('client {}'.format(data)), end='')
 
