6b9faac97f09bccf7c34b42cd89cdf383f8d4e88
py-am-i <parkeryoung228@gmail.com>
2017-03-29 16:45:22 -0700

    Added `connections.py` to the client distribution package code in `shell_plugins/Client.py`

--- shell_plugins/Client.py
+++ shell_plugins/Client.py
@@ -94,8 +94,9 @@ class Plugin(object):
             copy(origin, destination)
 
         copy(oyster_fp + '__init__.py', oyster_fp + 'dist/__init__.py')
-        copy(oyster_fp + 'common.py', oyster_fp + 'dist/common.py')
         copy(oyster_fp + 'client.py', oyster_fp + 'dist/client.py')
+        copy(oyster_fp + 'common.py', oyster_fp + 'dist/common.py')
+        copy(oyster_fp + 'connection.py', oyster_fp + 'dist/connection.py')
         copy(oyster_fp + 'update.py', oyster_fp + 'dist/update.py')
 
         print('\r< Finished. >')
