c7a337e4cdd907ce6fe48e8fe7db4a9662d86844
py-am-i <parkeryoung228@gmail.com>
2017-04-02 14:35:31 -0700

    Added more documentation and refactored the `Editor` plugin to use tagged filenames for more complicated editing.
    It used to be that the `Editor` plugin would only work if the command
    was structured like:
    
    `{program} {flags} {filename}`
    
    Now you just have to tag filenames with `<f>` so that they can be
    handled correctly.
    
    Example of using imagemagick to resize a picture:
    
        `/Users/user/Pictures> magick ~/Pictures/drink.jpg -resize 50%
    drink.jpg`
    
      becomes
    
        `/Users/user/Pictures> # magick <f>~/Pictures/drink.jpg -resize 50%
    <f>drink.jpg`
    
    If you ran the first example, it would try to use imagemagick on the
    client machine whereas the second example would grab the filedata(for
    the first tagged filename in the command), from the client machine and
    then use imagemagick to edit it locally before sending the modified
    data back to the client.

--- client_plugins/Editor.py
+++ client_plugins/Editor.py
@@ -5,28 +5,35 @@ from os.path import expanduser
 
 class Plugin(object):
     """
-    Editor v1.0 - Server-Side
+    Editor v1.0 - Client-Side
 
     This plugin is designed for using local server-side tools to edit client-side files!
 
     As it stands, only small files are supported.
 
-    Any file that can be opened and edited from the command line with the following syntax:
+    Any file that can be opened and edited from the command line should be editable!
 
-        > {program} {flags} {filepath}
-
-    Can be edited using the syntax:
-
-        > # {program} {flags} {filepath}
-
-    It is important that {program} is the first argument, after the `#`,
-    and {filepath} is the last argument.
-
-    If you have `suplemon` installed, you can use the syntax:
+    If you have `suplemon` installed locally, you can use the syntax:
 
         > # {filepath}
 
     to open any text file for editing without having to type the name of the editor.
+
+    For more complicated editing(audio, pictures, video, etc), you can tag the
+    filename in question with `<f> and provide more arguments.
+
+    Example of using imagemagick to resize a picture:
+
+        `/Users/user/Pictures> magick ~/Pictures/drink.jpg -resize 50% drink.jpg`
+
+      becomes
+
+        `/Users/user/Pictures> # magick <f>~/Pictures/drink.jpg -resize 50% <f>drink.jpg`
+
+    If you ran the first example, it would try to use imagemagick on the client machine
+    whereas the second example would grab the filedata(for the first tagged filename in
+    the command), from the client machine and then use imagemagick to edit it locally
+    before sending the modified data back to the client.
     """
 
     version = 'v1.0'
@@ -36,7 +43,11 @@ class Plugin(object):
     @staticmethod
     def run(client, data):
         args = shlex.split(data)
-
+        # Step 1.
+        # The initial command sent by the `outgoing_plugins/Editor.py` file is what
+        # triggered this plugin to run, so since the `outgoing_plugins/Editor.py`
+        # file expects a response, we need to send one before moving on to the
+        # next step.
         if not args:
             client.server_print('Did not specify a file to edit.')
 
@@ -58,14 +69,18 @@ class Plugin(object):
 
         # Tell the server that the file was read correctly.
         client.send_data('OK')
+        # Step 1 Done!
 
-        # Send the filename
+        # Step 2.
+        # Now the server expects a filename to be sent over.
         filename = filepath.split('/')[-1] if '/' in filepath else filepath.split('\\')[-1]
         client.send_data(filename)
 
+        # Step 3.
         # Send the original file data.
         client.send_data(file_data, encode=False)
 
+        # Step 4.
         # Receive the new file data.
         new_file_data = client.receive_data(decode=False)
 
@@ -75,4 +90,5 @@ class Plugin(object):
 
         # Tell the server the new file data has been written.
         client.send_data('OK')
+        # Step 4 Done!
         return
--- outgoing_plugins/Editor.py
+++ outgoing_plugins/Editor.py
@@ -1,3 +1,36 @@
+# ~*
+# This is a nice plugin to give a little overview of the basic plugin api.
+# The `client_plugins/Editor.py` file is the companion plugin to this file
+# so make sure to follow along there as well.
+#
+# `Plugin`s call their `run` method when commands are executed in certain
+# prompts in the Oyster Shell
+#
+# Any file dropped in a plugins folder will be treated as a plugin.
+# Any file dropped in a plugins folder must be contain object named
+# `Plugin`, and should inherit from `object`.  Each `Plugin` is
+# required to have the following static attributes:
+#
+#     version           -   The version of the plugin
+#     invocation        -   The command that invokes the `run` method
+#                            on the `Plugin` object.
+#     enabled           -   Whether the plugin is enabled(True or False)
+#
+# A `run` method is required as well.  It doesn't have to be static
+# but if it is then the `Plugin` doesn't have to be instantiated by
+# the Oyster Shell.  The `run` method must have either a `server`(or
+# `client` for client plugins) instance as the first argument, and
+# `data` as the second argument.  `data` will be any arguments that
+# were passed along with the command that invoked the `Plugin`.
+# It is up to the author of the plugin to handle parsing the
+# data.  I have a live template in pycharm so I can type `oyp`,
+# hit ENTER, and then start filling in the needed attributes.
+#
+# Read over the comments throughout the 2 `Editor.py` files.
+# I labeled some "Steps" to show how the plugins communicate
+# with each other.
+# ~*~*
+
 import shlex
 from os import remove
 from subprocess import Popen
@@ -11,46 +44,68 @@ class Plugin(object):
 
     As it stands, only small files are supported.
 
-    Any file that can be opened and edited from the command line with the following syntax:
+    Any file that can be opened and edited from the command line should be editable!
 
-        > {program} {flags} {filepath}
-
-    Can be edited using the syntax:
-
-        > # {program} {flags} {filepath}
-
-    It is important that {program} is the first argument, after the `#`,
-    and {filepath} is the last argument.
-
-    If you have `suplemon` installed, you can use the syntax:
+    If you have `suplemon` installed locally, you can use the syntax:
 
         > # {filepath}
 
     to open any text file for editing without having to type the name of the editor.
+
+    For more complicated editing(audio, pictures, video, etc), you can tag the
+    filename in question with `<f> and provide more arguments.
+
+    Example of using imagemagick to resize a picture:
+
+        `/Users/user/Pictures> magick ~/Pictures/drink.jpg -resize 50% drink.jpg`
+
+      becomes
+
+        `/Users/user/Pictures> # magick <f>~/Pictures/drink.jpg -resize 50% <f>drink.jpg`
+
+    If you ran the first example, it would try to use imagemagick on the client machine
+    whereas the second example would grab the filedata(for the first tagged filename in
+    the command), from the client machine and then use imagemagick to edit it locally
+    before sending the modified data back to the client.
     """
 
     version = 'v1.0'
     invocation = '# '
     enabled = True
+    editor = 'suplemon'
+    temp_filename = '_e_d_i_t__t_e_m_p_'
 
     @staticmethod
     def run(server, data):
         args = shlex.split(data, posix=False)
 
-        # Handle the args.
+        # Modify the args so that they can be passed to Popen.
+        # Any more than 2 arguments and tagged filenames are
+        # required.
         if len(args) > 2:
-            editor, filepath = args[0], args[-1]
+            filepaths = [fp[3:] for fp in args if fp.startswith('<f>')]
+            if not filepaths:
+                print('< Editing requires a filepath in order to function. >')
+                return
+            filepath = filepaths[0]
+        # Just 2 arguments means the editor will be the first argument.
         elif len(args) == 2:
-            editor, filepath = args
+            filepath = args[1]
+        # 1 arg means we need to open up the file with the editor
+        # set in the static variable Plugin.editor.
         elif len(args) == 1:
-            editor, filepath, flags = 'suplemon', args[0], []
-            args.insert(0, 'suplemon')
+            args.insert(0, Plugin.editor)
         else:
             print('< Editing requires a filepath in order to function. >')
             return
 
-        # Send the file name over to the client
-        r = server.send_command('{}{}'.format(Plugin.invocation, filepath))
+        # Remove the <f> tag from any argument, where it exists as the
+        # first 3 characters, if it exists.
+        untagged_filepath = filepath if filepath[:3] != '<f>' else filepath.replace('<f>', '', 1)
+
+        # Step 1.
+        # Send the file name over to the client and expect a response.
+        r = server.send_command('{}{}'.format(Plugin.invocation, untagged_filepath))
 
         # If the client doesn't respond with "OK"
         # then there was a problem opening the file
@@ -59,35 +114,29 @@ class Plugin(object):
             print(r)
             return
 
-        # Receive the filename.
-        temp_file_name = server.connection_mgr.current_connection.get_response()
-        if temp_file_name[:7] == 'ERROR: ':
-            print(temp_file_name)
+        # Step 2.
+        # Receive the remote filename.
+        remote_file_name = server.connection_mgr.current_connection.get_response()
+        if remote_file_name[:7] == 'ERROR: ':
+            print(remote_file_name)
             return
 
-        # Create a temporary file
-        temp_file_name = '_e_d_i_t__t_e_m_p_.{}'.format(temp_file_name.split('.')[-1])
+        # Create a temporary file using the remote file's extension if possible.
+        extension = remote_file_name.split('.')[-1] if '.' in remote_file_name else ''
+        if extension:
+            temp_file_name = '{}.{}'.format(Plugin.temp_filename, extension)
+        else:
+            temp_file_name = Plugin.temp_filename
 
-        # Get the file data.
+        # Step 3.
+        # Get the file data from the client.
         file_data = server.connection_mgr.current_connection.get_response(decode=False)
         with open(temp_file_name, 'wb') as f:
             f.write(file_data)
             f.close()
 
-        while True:
-            # Open editor process to edit the file.
-            # get the index of the filepath
-            try:
-                findex = args.index(filepath)
-            except ValueError:
-                break
-
-            # insert the temporary filename at
-            # the index.
-            args.insert(findex, temp_file_name)
-
-            # remove the original filepath
-            args.remove(filepath)
+        # Replace tagged file names with the temporary filename.
+        args = [arg if arg[:3] != '<f>' else temp_file_name for arg in args]
 
         # Open the process.
         p = Popen(args=args)
@@ -99,6 +148,8 @@ class Plugin(object):
         # made to the file client-side!
         with open(temp_file_name, 'rb') as f:
             new_file_data = f.read()
+            # Step 4.
+            # Send the new unencoded file data over to the client.
             r = server.send_command(new_file_data, encode=False)
             if r.strip() != 'OK':
                 print(r)
